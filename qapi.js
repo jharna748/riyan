0309dd36add16e1eca5d8bdfffea00aab987b7e0
jharna748 <77603642+jharna748@users.noreply.github.com>
2021-01-24 18:29:46 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"600d6e0dae0c05001e87a951",
+				"600d6e0dae0c05001e87a953"
+			]
+		},
+		"600d6e0dae0c05001e87a951": {
+			"id": "600d6e0dae0c05001e87a951",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"600d6e0dae0c05001e87a953": {
+			"id": "600d6e0dae0c05001e87a953",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
